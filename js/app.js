52279abaac133ffa0c5e50f8813abb88201b3379
Margui <margui.berjon@gmail.com>
2020-05-18 13:00:23 +0200

    reset button

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const email = document.getElementById('email');
 const subject = document.getElementById('asunto');
 const message = document.getElementById('mensaje');
 const btnSEnd = document.getElementById('enviar');
+const sendEmailForm = document.getElementById('enviar-mail');
+const btnReset = document.getElementById('resetBtn');
 
 
 document.addEventListener('DOMContentLoaded', btnDisable);
@@ -9,8 +11,8 @@ document.addEventListener('DOMContentLoaded', btnDisable);
 email.addEventListener('blur', btnEnable);
 subject.addEventListener('blur', btnEnable);
 message.addEventListener('blur', btnEnable);
-
-
+btnSEnd.addEventListener('click', sendEmail);
+btnReset.addEventListener('click', resetForm);
 
 function btnDisable() {
   btnSEnd.disabled = true;
@@ -50,4 +52,29 @@ function fieldValidated(input) {
     input.style.borderBottomColor = 'red';
     input.classList.add('error');
   }
+}
+
+function sendEmail(ev) {
+  const spinnerGif = document.querySelector('#spinner')
+  spinnerGif.style.display = 'block';
+
+  const sendEmailGif = document.createElement('img');
+  sendEmailGif.src = './img/mail.gif';
+  sendEmailGif.style.display = 'block';
+
+  setTimeout(function () {
+    spinnerGif.style.display = 'none';
+    document.querySelector('#loaders').appendChild(sendEmailGif);
+    setTimeout(function () {
+      sendEmailGif.remove();
+      sendEmailForm.reset();
+      btnDisable();
+    }, 4000)
+  }, 3000)
+  ev.preventDefault();
+}
+
+function resetForm(ev) {
+  ev.preventDefault();
+  sendEmailForm.reset();
 }
