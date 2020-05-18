3123624207d83b9f11d41e12fa7f8f6107621c7f
Margui <margui.berjon@gmail.com>
2020-05-18 11:30:55 +0200

    send button disable

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const email = document.getElementById('email');
+const asunto = document.getElementById('asunto');
+const message = document.getElementById('message');
+const btnSEnt = document.getElementById('enviar');
+
+
+document.addEventListener('DOMContentLoaded', btnDisable)
+
+
+
+
+
+
+function btnDisable() {
+  btnSEnt.disabled = true;
+}
