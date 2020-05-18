b8317f74732b1f671515b2604075c5c860a774ef
Margui <margui.berjon@gmail.com>
2020-05-18 11:53:40 +0200

    Enable send botton

--- js/app.js
+++ js/app.js
@@ -1,16 +1,37 @@
 const email = document.getElementById('email');
-const asunto = document.getElementById('asunto');
-const message = document.getElementById('message');
-const btnSEnt = document.getElementById('enviar');
+const subject = document.getElementById('asunto');
+const message = document.getElementById('mensaje');
+const btnSEnd = document.getElementById('enviar');
 
 
-document.addEventListener('DOMContentLoaded', btnDisable)
+document.addEventListener('DOMContentLoaded', btnDisable);
 
-
-
+email.addEventListener('blur', btnEnable);
+subject.addEventListener('blur', btnEnable);
+message.addEventListener('blur', btnEnable);
 
 
 
 function btnDisable() {
-  btnSEnt.disabled = true;
+  btnSEnd.disabled = true;
+}
+
+
+function btnEnable() {
+  fieldValidated(this);
+  if (email.value !== '' && subject.value !== '' && message.value !== '') {
+    btnSEnd.disabled = false;
+  }
+
+
+}
+
+function fieldValidated(input) {
+  if (input.value.length > 0) {
+    input.style.borderBottomColor = 'green';
+    input.classList.remove('error');
+  } else {
+    input.style.borderBottomColor = 'red';
+    input.classList.add('error');
+  }
 }
